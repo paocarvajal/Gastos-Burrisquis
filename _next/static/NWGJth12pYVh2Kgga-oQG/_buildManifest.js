e4812c9a76b039d3f417b339572cc9fc87bc75d7
Pao Carvajal <nailil811@gmail.com>
2025-11-28 21:20:25 -0600

    Add files via upload
    upload files and commit changes

---
+++ _next/static/NWGJth12pYVh2Kgga-oQG/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Gastos-Burrisquis//_next/:path+",
+        "destination": "/Gastos-Burrisquis/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
