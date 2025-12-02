56cb8b1f5c3d06f7b495c772a6d814a649e180cb
Paola <nailil811@gmail.com>
2025-12-02 14:06:18 -0600

    Fix paths

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,15 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const repoName = "Gastos-Burrisquis";
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
 };
-
-
-
 
 export default nextConfig;
